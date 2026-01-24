1e5eed4072a89c097f6b92046a9869df282306d8
Mohammed Tharick <mdtharick25@gmail.com>
2026-01-24 22:02:37 +0530

    last

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,10 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:4000/api",
+  baseURL:
+    import.meta.env.MODE === "production"
+      ? "/api"
+      : "http://localhost:4000/api",
 });
 
 api.interceptors.request.use((config) => {
